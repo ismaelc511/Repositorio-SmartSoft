212feefa7f15db016d854ee7d056934f0e5b21b1
Ismael Carvajal <ismaelcarvajal@outlook.com>
2023-01-23 15:15:46 -0500

    Subo cambios en el frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useContext} from 'react';
+import Register2 from "./components/Register2"
+import Login from "./components/Login"
+import Header from './components/Header';
 import './App.css';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { UserContext } from './context/userContext';
+import TaskManager from './components/TaskManager';
+
 
 function App() {
+
+  const [todoList, setTodoList] = useState([{}])
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+  const [token] = useContext(UserContext);
+
+  // Read all todos
+  useEffect(() =>{
+    axios.get('http://localhost:8000/api/todo')
+    .then(res => {
+      setTodoList(res.data)
+    })
+  });
+
+  //Read one todo
+  const getOneHandler = () =>{
+    axios.get(`http://localhost:8000/api/todo${title}`, {'title': title})
+    .then(response => response)
+  }
+
+  // Post a todo
+  const addTodoHandler = () => {
+    axios.post('http://localhost:8000/api/todo', {'title': title, 'description': desc})
+    .then(res2 => res2)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header /*title={message}*/ />
+    <div className='App list-group-item justify-content-center align-items-center mx-auto' style={{"width":"400px", "backgroundColor": "white", "marginTop":"15px"}}>
+      {
+        !token ? (
+          <div className='columns'>
+            <Register2 /> <Login />
+          </div>
+        ) : (
+          <div>
+            <TaskManager />
+          </div>
+        )
+        }
     </div>
+    </>
   );
 }
 
